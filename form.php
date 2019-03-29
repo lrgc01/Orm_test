c62922b8ecbe63aaa336780e26af3d8d7d364bfd
Luiz Carvalho <luizrogeriocarvalho@gmail.com>
2019-03-29 13:51:13 -0300

    Fix output.

--- form.php
+++ form.php
@@ -23,9 +23,9 @@ margin-right:auto;
 
 <?php
 // Some useful data
-echo "This server name: $_SERVER['SERVER_NAME'] // ";
-echo "This server address: $_SERVER['SERVER_ADDR'] // ";
-echo "Time of the request: $_SERVER['REQUEST_TIME'] // ";
+echo "This server name: $_SERVER['SERVER_NAME'] , ";
+echo "This server address: $_SERVER['SERVER_ADDR'] , ";
+echo "Time of the request: $_SERVER['REQUEST_TIME'] , ";
 ?>
 
 <HR>
