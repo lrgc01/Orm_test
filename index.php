85ba85b480a62a40b2c646d3d736e8bc321a6432
Luiz Carvalho <luizrogeriocarvalho@gmail.com>
2019-03-29 13:53:17 -0300

    Fix output 2.

--- index.php
+++ index.php
@@ -21,9 +21,9 @@
 
 <?php
 // Some useful data
-echo "This server name: $_SERVER['SERVER_NAME'] , ";
-echo "This server address: $_SERVER['SERVER_ADDR'] , ";
-echo "Time of the request: $_SERVER['REQUEST_TIME'] , ";
+echo "This server name: " . $_SERVER['SERVER_NAME'] . " - ";
+echo "This server address: " . $_SERVER['SERVER_ADDR'] . " - ";
+echo "Time of the request: " . $_SERVER['REQUEST_TIME'] . " - ";
 ?>
 
 <HR>
