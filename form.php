f8ffdde7c3c63d320f47409b34ecab53bff7ccb6
Luiz Carvalho <luizrogeriocarvalho@gmail.com>
2019-03-29 19:29:00 -0300

    Expanded form.php

--- form.php
+++ form.php
@@ -11,7 +11,7 @@ margin-right:auto;
 </style>
 
 <body>
-<H2>Your data</H2>
+<H2>Enter your data</H2>
 <HR>
 
 <?php
@@ -22,16 +22,38 @@ require_once("include/myfunctions.php");
 <?php
 // Use base function 'my_input_filter' to do a preliminary cleaning
 // The same 'my_input_filter' function certifies that '$name' is not null
-$name       = my_input_filter($_POST['name'],"<p>You must provide some content to the required field: Name.</p>");
+$name       = my_input_filter($_POST['name']);
 $favColor   = my_input_filter($_POST['favColor']);
 $catsOrDogs = my_input_filter($_POST['catsOrDogs']);
 ?>
 
 <HR>
+<!--
 Name: <?php echo $name; ?><br />
 Favorite Color: <?php echo $favColor; ?><br />
 Cats or dogs: <?php echo $catsOrDogs; ?><br />
 <br />
+-->
+
+<form action="form.php" method="post">
+<table border="0" cellpadding="5" cellspacing="1">
+ <tbody>
+  <tr>
+   <td> Name:           </td> <td> <input type="text" name="name" /></td>
+  </tr>
+  <tr>
+   <td> Favorite Color: </td> <td> <input type="text" name="favColor" /></td>
+  </tr>
+  <tr>
+   <td> Cats or Dogs:   </td> <td> <input type="text" name="catsOrDogs" /></td>
+  </tr>
+ </tbody>
+</table>
+
+<p><input type="submit" value="Submit"></p>
+</form>
+
+<HR>
 
 <?php
    // Just load database settings
@@ -39,20 +61,23 @@ Cats or dogs: <?php echo $catsOrDogs; ?><br />
 
    $mysqli_conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
-   $id = check_name($mysqli_conn,$name);
-
-   if ($id != 0) {
-      // Prepare statement and execute
-      $update_stmt = $mysqli_conn->prepare("UPDATE mainData set name=?, favColor=?, catsOrDogs=? WHERE id=?");
-      $update_stmt->bind_param("sssi", $name, $favColor, $catsOrDogs, $id); 
-      $update_stmt->execute(); 
-
-   } else {
-
-      // Prepare statement and execute
-      $insert_stmt = $mysqli_conn->prepare("INSERT INTO mainData (id, name, favColor, catsOrDogs) values ('', ?, ?, ?)");
-      $insert_stmt->bind_param("sss", $name, $favColor, $catsOrDogs); 
-      $insert_stmt->execute(); 
+   if ($name != '') 
+   {
+      $id = check_name($mysqli_conn,$name);
+   
+      if ($id != 0) {
+         // Prepare statement and execute
+         $update_stmt = $mysqli_conn->prepare("UPDATE mainData set name=?, favColor=?, catsOrDogs=? WHERE id=?");
+         $update_stmt->bind_param("sssi", $name, $favColor, $catsOrDogs, $id); 
+         $update_stmt->execute(); 
+   
+      } else {
+   
+         // Prepare statement and execute
+         $insert_stmt = $mysqli_conn->prepare("INSERT INTO mainData (id, name, favColor, catsOrDogs) values ('', ?, ?, ?)");
+         $insert_stmt->bind_param("sss", $name, $favColor, $catsOrDogs); 
+         $insert_stmt->execute(); 
+      }
    }
 
    print_current_data($mysqli_conn);
