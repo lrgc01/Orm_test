e8c41cfb4f2164e975bf734ae5e4c13c750afafc
Luiz Carvalho <luizrogeriocarvalho@gmail.com>
2019-03-29 20:41:16 -0300

    Change date output format

--- include/useful_data.php
+++ include/useful_data.php
@@ -2,6 +2,6 @@
 // Some useful data
 echo "<I>This server name: </I>" . $_SERVER['SERVER_NAME'] . " - ";
 echo "<I>This server address: </I>" . $_SERVER['SERVER_ADDR'] . " - ";
-echo "<I>Time of the request: </I>" . $_SERVER['REQUEST_TIME'] . "</ BR>";
+echo "<I>Time of the request: </I>" . date("D M j G:i:s T Y", $_SERVER['REQUEST_TIME']) . "</ BR>";
 ?>
 
