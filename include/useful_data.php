28c3bb077dcf7a91509ae91f8007c91d87e71a8a
Luiz R G de Carvalho <luizrogeriocarvalho@gmail.com>
2019-03-30 11:48:35 -0300

    Changed 'address' to 'local address' in output

--- include/useful_data.php
+++ include/useful_data.php
@@ -1,7 +1,7 @@
 <?php
 // Some useful data
 echo "<I>This server name: </I>" . $_SERVER['SERVER_NAME'] . " - ";
-echo "<I>This server address: </I>" . $_SERVER['SERVER_ADDR'] . " - ";
+echo "<I>This server local address: </I>" . $_SERVER['SERVER_ADDR'] . " - ";
 echo "<I>Time of the request: </I>" . date("D M j G:i:s T Y", $_SERVER['REQUEST_TIME']) . "</ BR>";
 ?>
 
