c19e61e9dbff9b5b236c78e34661a030fccd63bd
Luiz R G de Carvalho <luizrogeriocarvalho@gmail.com>
2019-03-30 20:27:59 -0300

    Added local hostname to useful data

--- include/useful_data.php
+++ include/useful_data.php
@@ -1,6 +1,6 @@
 <?php
 // Some useful data
-echo "<I>This HTTP server host: </I>" . $_SERVER['HTTP_HOST'] . " - ";
+echo "<I>This server hostname: </I>" . gethostname() . " - ";
 echo "<I>This server local address: </I>" . $_SERVER['SERVER_ADDR'] . " - ";
 echo "<I>Time of the request: </I>" . date("D M j G:i:s T Y", $_SERVER['REQUEST_TIME']) . "</ BR>";
 ?>
