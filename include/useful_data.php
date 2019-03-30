00bf2a8d6624ee71dae41eb5e3fe28736f1077e2
Luiz R G de Carvalho <luizrogeriocarvalho@gmail.com>
2019-03-30 20:14:41 -0300

    Changed host displayed

--- include/useful_data.php
+++ include/useful_data.php
@@ -1,6 +1,6 @@
 <?php
 // Some useful data
-echo "<I>This server name: </I>" . $_SERVER['SERVER_NAME'] . " - ";
+echo "<I>This HTTP server host: </I>" . $_SERVER['HTTP_HOST'] . " - ";
 echo "<I>This server local address: </I>" . $_SERVER['SERVER_ADDR'] . " - ";
 echo "<I>Time of the request: </I>" . date("D M j G:i:s T Y", $_SERVER['REQUEST_TIME']) . "</ BR>";
 ?>
