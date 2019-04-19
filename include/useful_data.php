a1d866e9e84fbb1d31b7c94377f397740c1b4938
Luiz Carvalho <luizrogeriocarvalho@gmail.com>
2019-04-19 15:52:03 +0000

    Changed to display PHP server hostname.

--- include/useful_data.php
+++ include/useful_data.php
@@ -1,7 +1,7 @@
 <?php
 // Some useful data
-//echo "<I>Server hostname: </I>" . gethostname() . " - ";
-echo "<I>Server hostname: </I>" . $_SERVER['HTTP_HOST'] . " - ";
+//echo "<I>Server hostname: </I>" . $_SERVER['HTTP_HOST'] . " - ";
+echo "<I>PHP server hostname: </I>" . gethostname() . " - ";
 echo "<I>Server local address: </I>" . $_SERVER['SERVER_ADDR'] . " - ";
 echo "<I>Time of the request: </I>" . date("D M j G:i:s T Y", $_SERVER['REQUEST_TIME']) . "</ BR>";
 ?>
