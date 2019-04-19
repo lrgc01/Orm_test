454fc94e0811693920c1e4a0273c655c064e336f
Luiz Carvalho <luizrogeriocarvalho@gmail.com>
2019-04-19 15:54:06 +0000

    added HTTP to the server name explanation

--- include/useful_data.php
+++ include/useful_data.php
@@ -2,7 +2,7 @@
 // Some useful data
 //echo "<I>Server hostname: </I>" . $_SERVER['HTTP_HOST'] . " - ";
 echo "<I>PHP server hostname: </I>" . gethostname() . " - ";
-echo "<I>Server local address: </I>" . $_SERVER['SERVER_ADDR'] . " - ";
+echo "<I>HTTP server local address: </I>" . $_SERVER['SERVER_ADDR'] . " - ";
 echo "<I>Time of the request: </I>" . date("D M j G:i:s T Y", $_SERVER['REQUEST_TIME']) . "</ BR>";
 ?>
 
