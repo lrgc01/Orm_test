9b7406638606d96d5a1da500e2e53a27bd1020a7
Luiz Carvalho <luizrogeriocarvalho@gmail.com>
2019-04-19 15:21:03 +0000

    Changed gethostname by _SERVER['SERVER_NAME']

--- include/useful_data.php
+++ include/useful_data.php
@@ -1,6 +1,7 @@
 <?php
 // Some useful data
-echo "<I>Server hostname: </I>" . gethostname() . " - ";
+//echo "<I>Server hostname: </I>" . gethostname() . " - ";
+echo "<I>Server hostname: </I>" . $_SERVER['SERVER_NAME'] . " - ";
 echo "<I>Server local address: </I>" . $_SERVER['SERVER_ADDR'] . " - ";
 echo "<I>Time of the request: </I>" . date("D M j G:i:s T Y", $_SERVER['REQUEST_TIME']) . "</ BR>";
 ?>
